47b471a341bff9d5b5a077875cf707fb6a245555
dtandev <dtandev@gmail.com>
2019-08-15 08:56:30 +0200

    dbManager without examples
    I removed examples of datasets and examples of some functions since they caused errors during module import.

--- src/dbManager.py
+++ src/dbManager.py
@@ -35,24 +35,9 @@ def createColumn(tableName, columnName, columnType):
     ALTER TABLE {}
     ADD {} {}
     """.format(tableName, columnName, columnType)
-    
     activeDB.execute(sqlCreateColumnSyntax)
     db.commit()
 
-
-### create dbHeaders example ###
-dbHeaders = {
-    'date':'DATE', 
-    'category':'VARCHAR(30)',
-    'title':'VARCHAR(200)', 
-    'text':'VARCHAR(8000)',
-    'author':'VARCHAR(40)'
-}
-
-for header in dbHeaders:
-    createColumn('books', header, dbHeaders[header])
-
-### end example ###
 
 def setPrimaryKey(tableName, columnName):
     sqlPrimatyKeySyntx = """
@@ -84,17 +69,7 @@ def insertInTable(tableName, values):
     INSERT INTO {}
     VALUES (?, ?, ?, ?, ?) 
     """.format(tableName)
-     
     activeDB.executemany(sqlInsertIntoSyntax, values)
-
-### example of data set ###
-
-values = [(3, '2019-08-13 13:00:00', "sport", "Lewy blysnal", "Lewandowski strzelił 2 bramki Enerdze Cottbus"), 
-          (4, '2019-08-13 13:10:00', "sport", "Milik nie zagral", "Arkadusz Milik nie wzial udziału w meczu towarzystkim z powodu kontuzji kolana")]
-
-insertInTable('news', values)
-
-### end example ###
 
 
 def inputSqlStatement():
