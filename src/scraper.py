2e752142f66b67eb4f3447f266b667728726c4f1
igor <igorw645@gmail.com>
2019-08-18 19:30:51 +0200

    Work version 0.0.1

--- src/scraper.py
+++ src/scraper.py
@@ -9,20 +9,75 @@ from bs4 import BeautifulSoup
 #lxml - pip
 
 
-user_Agent = generate_user_agent()
-heders = {"user-agent": user_Agent}
+# user_Agent = generate_user_agent()
+# heders = {"user-agent": user_Agent}
 
-r = requests.get("http://gazetaolsztynska.pl/sport-w-Olsztynie/4-0.html")
+ARTICLELIST =[]
 
-soup = BeautifulSoup(r.text, "lxml")
+class Article:
+    def __init__(self, Title, Date, Teaser, Content):
+        self.Title = Title
+        self.Date = Date
+        self.Teaser = Teaser
+        self.Content = Content
 
-body = soup.body
-ContainerNews = body.find('div',{'class': 'block-fullnews'})
-ListNew = ContainerNews.find_all('li')
+def GetListArticleOnPage(xUriPageArticle):
+   
+    pRequest = requests.get(xUriPageArticle)
+    
+    if(pRequest.status_code > 200): 
+        return ""
+        
+    pSoup = BeautifulSoup(pRequest.text, "lxml")
+    pContainer = pSoup.body.find('div',{'class': 'block-fullnews'})
+    pArticleContentList =pContainer.find_all('li')
 
-header = ListNew[0].find('a',{'class': 'fl'}).get('title')
-a2 = ListNew[0].find_all('a')
-data = ListNew[0].find('p',{'class': 'block-fullnews-date'}).text
-ContentLink = ListNew[0].find('a',{'class': 'fl'}).get('href')
-zajawka = a2[2].text
-print("stop")
+    return pArticleContentList
+
+def GetArticlesFromArticleList(xArticleList):
+    
+    for pArticle in xArticleList:
+        try:
+            pTitle = pArticle.find('a',{'class': 'fl'}).get('title')
+            pA_Attributes = pArticle.find_all('a')
+            pData = pArticle.find('p',{'class': 'block-fullnews-date'}).text
+            pTeaser = pA_Attributes[2].text
+            pContentLink = pArticle.find('a',{'class': 'fl'}).get('href')
+            pContenArticle = GetContentArticle(pContentLink)
+            pArticle = Article(pTitle,pData,pTeaser,pContenArticle)
+            ARTICLELIST.append(pArticle)
+        except:
+            print("bat article shema")
+   
+
+
+def GetContentArticle(xUrl):
+
+    foo ="http://sport.wm.pl" in xUrl
+    if(foo == False):
+        print("Bad url to content")
+        return "Bad url to content"
+
+    pRequest = requests.get(xUrl) 
+    
+    if(pRequest.status_code > 200): 
+        return ""
+
+    pSoup = BeautifulSoup(pRequest.text, "lxml")
+    pContentAricle = pSoup.body.find('div',{'class': 'art-text intextAd'}).text
+    
+    return pContentAricle
+ 
+def PaginationArticle():
+    for PginIndex in range(320):
+        pCurrentPageArticleLists = GetListArticleOnPage("http://gazetaolsztynska.pl/sport-w-Olsztynie/4-"+str(PginIndex)+ ".html")
+        print("current page", PginIndex)
+        GetArticlesFromArticleList(pCurrentPageArticleLists)
+        print("Artykułow w liscie jest:", len(ARTICLELIST))
+
+    # pCurrentPageArticleLists = GetListArticleOnPage("http://gazetaolsztynska.pl/sport-w-Olsztynie/4-4.html")
+    # GetArticlesFromArticleList(pCurrentPageArticleLists)
+    # print(end)
+
+PaginationArticle()
+
