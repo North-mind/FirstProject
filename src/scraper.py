49a1f943b321a0239e3eca28c8e9d2daac2449a4
igor <igorw645@gmail.com>
2019-08-18 15:50:51 +0200

    Prace nad parsowaniem gazety olsztnskiej

--- src/scraper.py
+++ src/scraper.py
@@ -12,9 +12,17 @@ from bs4 import BeautifulSoup
 user_Agent = generate_user_agent()
 heders = {"user-agent": user_Agent}
 
-r = requests.get("http://gazetaolsztynska.pl/sport-w-Olsztynie/4-0.html", heders = heders)
+r = requests.get("http://gazetaolsztynska.pl/sport-w-Olsztynie/4-0.html")
 
 soup = BeautifulSoup(r.text, "lxml")
 
+body = soup.body
+ContainerNews = body.find('div',{'class': 'block-fullnews'})
+ListNew = ContainerNews.find_all('li')
 
-print(r.text)
+header = ListNew[0].find('a',{'class': 'fl'}).get('title')
+a2 = ListNew[0].find_all('a')
+data = ListNew[0].find('p',{'class': 'block-fullnews-date'}).text
+ContentLink = ListNew[0].find('a',{'class': 'fl'}).get('href')
+zajawka = a2[2].text
+print("stop")
